29e65c1db6b97231cb147d16238da852fc13b93a
joe-sacco <102517425+joe-sacco@users.noreply.github.com>
2023-05-03 00:36:12 +0900

    feat: Add TimestampedModel class

--- kataru_api/models.py
+++ kataru_api/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
+
+class TimestampedModel(models.Model):
+    created_at = models.DateTimeField(default=timezone.now, editable=False)
+    modified_at = models.DateTimeField(default=timezone.now, editable=True)
+
+    class Meta:
+        abstract = True
+
+
