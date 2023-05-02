37adee13f18090b72cbb745990a459e4a5c66fea
joe-sacco <102517425+joe-sacco@users.noreply.github.com>
2023-05-03 01:20:24 +0900

    feat: Add User model

--- kataru_api/models.py
+++ kataru_api/models.py
@@ -23,3 +23,13 @@ class Prompt(TimestampedModel):
 
     class Meta:
         db_table = 'prompt'
+
+class User(TimestampedModel):
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+    user_name = models.CharField(max_length=30)
+    password = models.CharField(max_length=30)
+    email = models.CharField(max_length=30)
+
+    class Meta:
+        db_table = 'user_' #trailing underscore to avoid keyword conflict
