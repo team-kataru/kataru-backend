de71b26b3ec7872cece71d71796ec036c10936bd
joe-sacco <102517425+joe-sacco@users.noreply.github.com>
2023-05-07 22:15:48 +0900

    feat: Add GenreSerializer to serializers.py

---
+++ kataru_api/serializers.py
@@ -0,0 +1,25 @@
+from rest_framework import serializers
+from .models import Genre, Prompt, User
+
+"""Create CRUD serializers for each model"""
+
+class GenreSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Genre
+        fields = ('level', 'category', 'created_at', 'updated_at')
+
+        def create(self, validated_data):
+            return Genre.objects.create(**validated_data)
+        
+        def retrieve(self, instance):
+            return instance
+        
+        def update(self, instance, validated_data):
+            instance.level = validated_data.get('level', instance.level)
+            instance.category = validated_data.get('category', instance.category)
+            instance.modified_at = validated_data.get('modified_at', instance.modified_at)
+            instance.save()
+            return instance
+        
+        def delete(self, instance):
+            instance.delete()
