88db401228030bb3956e54e58bf1eab37efcc948
joe-sacco <102517425+joe-sacco@users.noreply.github.com>
2023-05-07 23:44:09 +0900

    feat: Add PromptSerializer to serializers.py

--- kataru_api/serializers.py
+++ kataru_api/serializers.py
@@ -23,3 +23,26 @@ class GenreSerializer(serializers.ModelSerializer):
         
         def delete(self, instance):
             instance.delete()
+
+class PromptSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Prompt
+        fields = ('prompt_text', 'genre', 'created_at', 'updated_at')
+
+        def create(self, validated_data):
+            return Prompt.objects.create(**validated_data)
+        
+        def retrieve(self, instance):
+            return instance
+        
+        def update(self, instance, validated_data):
+            instance.prompt_text = validated_data.get('prompt_text', instance.prompt_text)
+            instance.genre = validated_data.get('genre', instance.genre)
+            instance.updated_at = validated_data.get('updated_at', instance.updated_at)
+            instance.save()
+            return instance
+        
+        def delete(self, instance):
+            instance.delete()
+
+            
