7e0fba435cc07dfd0bdfc081ace712494b001e9f
joe-sacco <102517425+joe-sacco@users.noreply.github.com>
2023-05-07 23:43:54 +0900

    feat: Add prompts url to urls.py

--- kataru_api/urls.py
+++ kataru_api/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
     # path('', views.kataru_api, name='kataru_api'),
     path('hello_world/', views.hello_world, name='hello_world'),
-    path('genres/', views.genres, name='genres')
+    path('genres/', views.genres, name='genres'),
+    path('prompts/', views.prompts, name='prompts')
 ]
