9dcfdc8ab6b92a17e3bb31770559ce2d8d5fd293
joe-sacco <102517425+joe-sacco@users.noreply.github.com>
2023-05-07 22:13:48 +0900

    feat: Add genres view in views.py

--- kataru_api/views.py
+++ kataru_api/views.py
@@ -11,4 +11,23 @@ Test View
 def hello_world(request):
     return Response({'message': 'Hello, world!'})
 
+"""
+Genres View
+"""
 
+@api_view(['GET', 'POST'])
+def genres(request):
+    """
+    List all genres or create a new genre.
+    """
+    if request.method == 'GET':
+        genres = Genre.objects.all()
+        serializer = GenreSerializer(genres, many=True)
+        return Response(serializer.data)
+    
+    elif request.method == 'POST':
+        serializer = GenreSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=201)
+        return Response(serializer.errors, status=400)
