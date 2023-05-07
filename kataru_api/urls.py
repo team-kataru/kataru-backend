a1902e5ac4031e551637838f21f9b5b7a5552253
joe-sacco <102517425+joe-sacco@users.noreply.github.com>
2023-05-07 22:08:57 +0900

    feat: Add genre url in urls.py

---
+++ kataru_api/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    # path('', views.kataru_api, name='kataru_api'),
+    path('hello_world/', views.hello_world, name='hello_world'),
+    path('genres/', views.genres, name='genres')
+]
