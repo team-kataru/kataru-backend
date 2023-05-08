2e245b36a30222992d6ddc9f52cf6b2b2c8a62d0
joe-sacco <102517425+joe-sacco@users.noreply.github.com>
2023-05-08 16:05:03 +0900

    feat: Add users_id path to urls.py

--- kataru_api/urls.py
+++ kataru_api/urls.py
@@ -6,5 +6,8 @@ urlpatterns = [
     path('hello_world/', views.hello_world, name='hello_world'),
     path('genres/', views.genres, name='genres'),
     path('prompts/', views.prompts, name='prompts'),
-    path('users/', views.users, name='users')
+    path('users/', views.users, name='users'),
+    path('users/<int:pk>/', views.users_id, name='users_id'),
+    path('users/<int:pk>/update/', views.users_id, name='users_id_update'), 
+    path('users/<int:pk>/delete/', views.users_id, name='users_id_delete')
 ]
