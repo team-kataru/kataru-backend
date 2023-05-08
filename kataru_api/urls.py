3b5f34f116cd08f4b0f2d6b46db5084611e40d9a
joe-sacco <102517425+joe-sacco@users.noreply.github.com>
2023-05-08 18:41:59 +0900

    feat: Add prompts_id path to urls.py

--- kataru_api/urls.py
+++ kataru_api/urls.py
@@ -6,11 +6,8 @@ urlpatterns = [
     path('hello_world/', views.hello_world, name='hello_world'),
     path('genres/', views.genres, name='genres'),
     path('genres/<int:pk>/', views.genres_id, name='genres_id'),
-    path('genres/<int:pk>/update/', views.genres_id, name='genres_id_update'),
-    path('genres/<int:pk>/delete/', views.genres_id, name='genres_id_delete'),
     path('prompts/', views.prompts, name='prompts'),
+    path('prompts/<int:pk>/', views.prompts_id, name='prompts_id'),
     path('users/', views.users, name='users'),
     path('users/<int:pk>/', views.users_id, name='users_id'),
-    path('users/<int:pk>/update/', views.users_id, name='users_id_update'), 
-    path('users/<int:pk>/delete/', views.users_id, name='users_id_delete')
 ]
